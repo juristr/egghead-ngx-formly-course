c9d4b608e701d5f2ed0522179cd74f2d86bef948
Juri <juri.strumpflohner@gmail.com>
2019-12-28 22:15:28 +0100

    feat: add some simple input field binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  form = new FormGroup({});
+  model = {
+    firstname: 'Juri',
+    age: 34
+  };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'firstname',
+      type: 'input',
+      templateOptions: {
+        label: 'Firstname'
+      }
+    }
+  ];
+
+  onSubmit({ valid, value }) {
+    console.log(value);
+  }
+}
