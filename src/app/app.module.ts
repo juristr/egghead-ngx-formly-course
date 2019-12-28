9dcc1156fad6e2aa53a505def99bafb874358baa
Juri <juri.strumpflohner@gmail.com>
2019-12-28 21:42:01 +0100

    build: install formly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
