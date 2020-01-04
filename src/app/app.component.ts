d93048e4b698078b3e78017df27096adf243613c
Juri <juri.strumpflohner@gmail.com>
2020-01-04 21:55:39 +0100

    advanced layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,13 @@ import { DataService } from './core/data.service';
 import { switchMap, startWith, tap } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
+const formlyRow = (fieldConfig: FormlyFieldConfig[]) => {
+  return {
+    fieldGroupClassName: 'display-flex',
+    fieldGroup: fieldConfig
+  };
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -27,83 +34,93 @@ export class AppComponent {
     {
       key: 'id'
     },
-    {
-      key: 'firstname',
-      type: 'input',
-      templateOptions: {
-        label: 'Firstname',
-        required: true
-      }
-    },
-    {
-      key: 'age',
-      type: 'input',
-      templateOptions: {
-        type: 'number',
-        label: 'Age',
-        min: 18
+    formlyRow([
+      {
+        key: 'firstname',
+        type: 'input',
+        className: 'flex-3',
+        templateOptions: {
+          label: 'Firstname',
+          required: true
+        }
       },
-      validation: {
-        messages: {
-          min: 'Sorry, you have to be older than 18'
+      {
+        key: 'age',
+        type: 'input',
+        className: 'flex-1',
+        templateOptions: {
+          type: 'number',
+          label: 'Age',
+          min: 18
+        },
+        validation: {
+          messages: {
+            min: 'Sorry, you have to be older than 18'
+          }
         }
       }
-    },
-    {
-      key: 'nationId',
-      type: 'my-autocomplete',
-      // type: 'select', // <select>
-      templateOptions: {
-        label: 'Nation',
-        options: this.dataService.getNations()
-      }
-    },
-    {
-      key: 'cityId',
-      type: 'select', // <select>
-      templateOptions: {
-        label: 'Cities',
-        options: []
+    ]),
+    formlyRow([
+      {
+        key: 'nationId',
+        type: 'select', // <select>
+        className: 'flex-3',
+        templateOptions: {
+          label: 'Nation',
+          options: this.dataService.getNations()
+        }
       },
-      expressionProperties: {
-        'templateOptions.disabled': model => !model.nationId,
-        'model.cityId': '!model.nationId ? null : model.cityId'
-      },
-      hideExpression: model => !model.nationId,
-      hooks: {
-        onInit: (field: FormlyFieldConfig) => {
-          field.templateOptions.options = field.form
-            .get('nationId')
-            .valueChanges.pipe(
-              startWith(this.model.nationId),
-              switchMap(nationId => this.dataService.getCities(nationId))
-            );
+      {
+        key: 'cityId',
+        type: 'select', // <select>
+        className: 'flex-3',
+        templateOptions: {
+          label: 'Cities',
+          options: []
+        },
+        expressionProperties: {
+          'templateOptions.disabled': model => !model.nationId,
+          'model.cityId': '!model.nationId ? null : model.cityId'
+        },
+        hideExpression: model => !model.nationId,
+        hooks: {
+          onInit: (field: FormlyFieldConfig) => {
+            field.templateOptions.options = field.form
+              .get('nationId')
+              .valueChanges.pipe(
+                startWith(this.model.nationId),
+                switchMap(nationId => this.dataService.getCities(nationId))
+              );
+          }
         }
       }
-    },
-    {
-      key: 'ip',
-      type: 'input',
-      templateOptions: {
-        label: 'IP Address',
-        required: true
-      },
-      validators: {
-        // validation: ['ip']
-        ip2: {
-          expression: c => !c.value || /(\d{1,3}\.){3}\d{1,3}/.test(c.value),
-          message: (errorr, field: FormlyFieldConfig) =>
-            `"${field.formControl.value}" is not valid`
+    ]),
+    formlyRow([
+      {
+        key: 'ip',
+        type: 'input',
+        className: 'flex-6',
+        templateOptions: {
+          label: 'IP Address',
+          required: true
+        },
+        validators: {
+          // validation: ['ip']
+          ip2: {
+            expression: c => !c.value || /(\d{1,3}\.){3}\d{1,3}/.test(c.value),
+            message: (errorr, field: FormlyFieldConfig) =>
+              `"${field.formControl.value}" is not valid`
+          }
         }
       }
-    }
+    ])
   ];
 
   constructor(
     private dataService: DataService,
     private translate: TranslateService
   ) {
-    this.translate.use('de');
+    // this.translate.use('de');
   }
 
   onSubmit({ valid, value }) {
